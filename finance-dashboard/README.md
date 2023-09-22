99f685f82163dc8b6c6a3150223b182fa585cf66
Ja-Crispy <77791242+Ja-Crispy@users.noreply.github.com>
2023-09-22 18:17:25 +0530

    1st evaluation

---
+++ finance-dashboard/README.md
@@ -0,0 +1,16 @@
+---
+title: Finance Dashboard
+emoji: 🦀
+colorFrom: pink
+colorTo: gray
+sdk: streamlit
+sdk_version: 1.17.0
+app_file: app.py
+pinned: false
+license: apache-2.0
+duplicated_from: rstallman/finance-dashboard
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
+
+The code is derived from https://github.com/hfwittmann/financial-timeseries-dashboard/tree/kedro-based
---
+++ finance-dashboard/subs/access_backend.py
@@ -0,0 +1,43 @@
+# from subs.orderdict import orderByValue
+import requests
+import json
+import streamlit as st
+import pandas as pd
+
+from environs import Env
+
+env = Env()
+env.read_env()
+
+# @st.cache
+def get_tickerlist():
+
+    response = requests.get(f"{env('HOST')}/Tickerlist_provider?datatype=tickerlist")
+
+    response_json = response.json()
+
+    companyTicker = pd.read_json(response_json["myData"])
+
+    return companyTicker
+
+
+# @st.cache
+def get_plot(selection: str, plottype: str):
+
+    assert plottype in ["scatter", "returns", "histogram"]
+
+    response = requests.get(
+        f"{env('HOST')}/Stockticker_provider?plottype={plottype}&selection={selection}"
+    )
+
+    blub = response.json()
+
+    fig = json.loads(blub["plot"])
+
+    return fig
+
+
+if __name__ == "__main__":
+    print(get_tickerlist())
+
+    print(get_plot("ADS.DE", "scatter"))
